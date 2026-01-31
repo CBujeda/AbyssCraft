3a77034e17f64f98aa1984ca607b3d5dae14518b
Clara Bujeda Muñoz <clarabujedamunoz@gmail.com>
2026-01-31 11:58:53 +0100

    Camara

--- include/core/Camera.h
+++ include/core/Camera.h
@@ -9,7 +9,28 @@ namespace AbyssCore{
     };
 
     class Camera{
+        public:
+            float c_Yaw = -90.0f;
+            float c_Pitch = 0.0f;
+            float c_Speed = 5.5f;
+            float c_Sensitivity = 0.1f;
+            float c_Zoom = 45.0f;
 
+            glm::vec3 c_Position;
+            glm::vec3 c_Front;
+            glm::vec3 c_Up;
+            glm::vec3 c_Right;
+            glm::vec3 c_WorldUp;
+
+            Camera(glm::vec3 position = glm::vec3(0.0f,0.0f,0.0f));
+
+            glm::mat4 GetViewMatrix();
+            void ProcessKeyboard(CameraMovement direction, float deltaTime);
+
+            void ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch = true);
+
+        private:
+            void updateCameraVectors();
     };
     
 } // namespace AbyssCore
---
+++ src/core/Camera.cpp
@@ -0,0 +1,73 @@
+#include "core/Camera.h"
+
+namespace AbyssCore{
+    
+    Camera::Camera(glm::vec3 position)
+    : c_Position(position),
+        c_Front(glm::vec3(0.0f,0.0f,-1.0f)),
+        c_WorldUp(glm::vec3(0.0f,1.0f,0.0f))
+    {
+        updateCameraVectors();
+    }
+
+    glm::mat4 Camera::GetViewMatrix(){
+        return glm::lookAt(c_Position, c_Position + c_Front, c_Up);
+    }
+
+    /**
+     * @brief Procesa la entrada del teclado para mover la posición de la cámara.
+     * 
+     * Calcula el desplazamiento basado en la dirección solicitada, la velocidad configurada y el tiempo transcurrido.
+     * 
+     * @param direction Enumeración que indica hacia dónde se debe mover la cámara (FORWARD, BACKWARD, etc.).
+     * @param deltaTime Tiempo transcurrido entre frames para garantizar un movimiento fluido e independiente de los FPS. No debe ser negativo.
+     * @return void
+     * @note El movimiento vertical (UP/DOWN) permite vuelo libre utilizando el vector 'Up' de la cámara.
+     */
+    void Camera::ProcessKeyboard(CameraMovement direction, float deltaTime){
+        float velocity = c_Speed * deltaTime;
+        if (direction == FORWARD)  c_Position += c_Front * velocity;
+        if (direction == BACKWARD) c_Position -= c_Front * velocity;
+        if (direction == LEFT)     c_Position -= c_Right * velocity;
+        if (direction == RIGHT)    c_Position += c_Right * velocity;
+        if (direction == UP)       c_Position += c_Up * velocity; // Vuelo libre
+        if (direction == DOWN)     c_Position -= c_Up * velocity;
+    }
+
+    /**
+     * @brief Procesa el movimiento del ratón para rotar la cámara.
+     * 
+     * Calcula los nuevos valores de Yaw (guiñada) y Pitch (cabeceo) basándose en el desplazamiento del ratón.
+     * 
+     * @param xoffset Desplazamiento horizontal del ratón.
+     * @param yoffset Desplazamiento vertical del ratón.
+     * @param constrainPitch Si es true, limita el cabeceo para evitar que la cámara se invierta (máximo 89 grados).
+     * @return void
+     * @note Es necesario llamar a esta función en cada evento de movimiento del ratón para mantener la fluidez.
+     */
+    void Camera::ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch) {
+        xoffset *= c_Sensitivity;
+        yoffset *= c_Sensitivity;
+
+        c_Yaw += xoffset;
+        c_Pitch += yoffset;
+
+        if (constrainPitch) {
+            if (c_Pitch > 89.0f)  c_Pitch = 89.0f;
+            if (c_Pitch < -89.0f) c_Pitch = -89.0f;
+        }
+        updateCameraVectors();
+    }
+
+
+
+    void Camera::updateCameraVectors() {
+        glm::vec3 front;
+        front.x = cos(glm::radians(c_Yaw)) * cos(glm::radians(c_Pitch));
+        front.y = sin(glm::radians(c_Pitch));
+        front.z = sin(glm::radians(c_Yaw)) * cos(glm::radians(c_Pitch));
+        c_Front = glm::normalize(front);
+        c_Right = glm::normalize(glm::cross(c_Front, c_WorldUp));
+        c_Up    = glm::normalize(glm::cross(c_Right, c_Front));
+    }
+}
